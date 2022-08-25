ced2827c9f0959e0d1d6a98e69a28b952f016b71
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-25 12:56:32 +0530

    add validations

--- src/main/java/com/spring/recipegradle/commands/RecipeCommand.java
+++ src/main/java/com/spring/recipegradle/commands/RecipeCommand.java
@@ -4,7 +4,12 @@ import com.spring.recipegradle.domain.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -17,17 +22,27 @@ import java.util.Set;
 public class RecipeCommand {
     private String id;
 
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String description;
 
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
 
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
 
+    @Min(1)
+    @Max(100)
     private Integer servings;
     private String source;
 
+    @URL
     private String url;
 
+    @NotBlank
     private String directions;
 
     private List<IngredientCommand> ingredients = new ArrayList<>();
