ad9eb40abbac05e212a8572950af9c2974bfb9bd
Kaveen Prabodhya <kaveen.djrock40@gmail.com>
2022-08-26 17:31:53 +0530

    mono<Void> to Mono<ResponseEntity<Void>> on deleteById

--- src/main/java/com/spring/recipegradle/services/IngredientService.java
+++ src/main/java/com/spring/recipegradle/services/IngredientService.java
@@ -1,10 +1,11 @@
 package com.spring.recipegradle.services;
 
 import com.spring.recipegradle.commands.IngredientCommand;
+import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Mono;
 
 public interface IngredientService {
     Mono<IngredientCommand> findByRecipeIdAndIngredientId(String recipeId, String ingredientId);
     Mono<IngredientCommand> saveIngredientCommand(IngredientCommand command);
-    Mono<Void> deleteById(String recipeId, String idToDelete);
+    Mono<ResponseEntity<Void>> deleteById(String recipeId, String idToDelete);
 }
